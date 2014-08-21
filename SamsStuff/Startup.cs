98528314b2b2fe1d8daa5fba8e5fbfa82eb8e44b
logicart <jrussell@logicartsolutions.com>
2014-08-21 13:56:34 -0700

    init

---
+++ SamsStuff/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SamsStuff.Startup))]
+namespace SamsStuff
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
